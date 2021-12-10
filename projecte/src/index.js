8a4ba1728f0df10e600f9b652c31785d1eae218f
xpardo <xpullodsmx@gmail.com>
2021-12-10 20:26:09 +0100

    Primera versio v0.2

--- projecte/src/index.js
+++ projecte/src/index.js
@@ -24,26 +24,3 @@ let div = document.createElement('div');
 div.innerHTML=footer;
 document.body.append(div);
 
-
-
-
-import { AssetsList } from "./_common/js/classes/assets-list-class.js"
-
-let assets = new AssetsList();
- 
-
-import { UsuarisList } from "./_common/js/classes/usuaris-list-class.js"
-
-
-let user = new UsuarisList();
-
-
-import { creaHTMLFormulaariAfegir } from './js/componentes';
-
-
-let cos = document.createElement('div');
-    cos.id ="divafeigir"
-    cos.className="container w-50"
-    cos.innerHTML=creaHTMLFormulaariAfegir(user,assets)
-    document.body.append(cos)
-
