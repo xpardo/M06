26abc555293f99e7658ba03238c0b12b87f11127
xpardo <xpullodsmx@gmail.com>
2022-03-02 21:12:50 +0100

    chuck norris

--- React/react-hello/src/HelloWoll.js
+++ React/react-hello/src/HelloWoll.js
@@ -1,17 +1,19 @@
 import Hello from "./Hello"
 
 function HelloWorld() {
-           
+       
     return Hello.map((contacts, index) => {
        const { card, name, avatar, phone, email } = contacts 
        return (
-          <tr key={card}>
-             <td>{card}</td>
-             <td>{name}</td>
-             <td>{avatar}</td>
-             <td>{phone}</td>
-             <td>{email}</td>
-          </tr>
+         <table>  
+            <tr key={card}>
+               <td>{card}</td>
+               <td>{name}</td>
+               <td>{avatar}</td>
+               <td>{phone}</td>
+               <td>{email}</td>
+            </tr>
+          </table> 
        )
     })
 
