26991077a14dda7ab3eaf9535519a774986065b1
xpardo <xpullodsmx@gmail.com>
2022-03-18 21:11:02 +0100

    crud ticket sencill

---
+++ Tickets_Cud_sencill/src/Common/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./styles.css"
+
+const Footer = () => {
+  return (
+
+    <div class="baix">
+
+      <hr></hr>
+      <h2 align="center">Peu de la meva aplicació</h2>
+    
+    
+    </div>
+  )
+}
+
+export default Footer
